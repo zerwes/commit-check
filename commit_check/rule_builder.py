69ca7f71f24300ca9403e060abdaeed8f75ca9cb
Klaus Zerwes <zerwes@users.noreply.github.com>
2026-07-06 11:45:49 +0200

    Revert "author validatioon rule : improved error message"
    This reverts commit 4103a1c678980aa04923af9ebb0593bcbed5b4b1.

--- commit_check/rule_builder.py
+++ commit_check/rule_builder.py
@@ -246,7 +246,7 @@ class RuleBuilder:
         return ValidationRule(
             check=catalog_entry.check,
             regex=regex,
-            error=catalog_entry.error + " - should match " + regex,
+            error=catalog_entry.error,
             suggest=catalog_entry.suggest,
         )
 
